7ed4b4836fd2f08f2de717495d1c177b1a8d1365
Shubham Agdari <shubham.agdari@gmail.com>
2025-01-30 17:40:54 +0530

    completed with Reactive Programming

--- src/components/model/CounterModel.js
+++ src/components/model/CounterModel.js
@@ -2,16 +2,16 @@ import { BehaviorSubject, merge, interval, EMPTY } from "rxjs";
 import { map, scan, startWith, switchMap } from "rxjs/operators";
 
 
-const count$ = new BehaviorSubject(0); // Holds the current count
-const inc$ = new BehaviorSubject(() => {}); // Emits increment operations
-const dec$ = new BehaviorSubject(() => {}); // Emits decrement operations
-const reset$ = new BehaviorSubject(() => {}); // Emits reset operations
-const autoInc$ = new BehaviorSubject(false); // Holds the auto-increment toggle state
+const count$ = new BehaviorSubject(0); 
+const inc$ = new BehaviorSubject(() => {}); 
+const dec$ = new BehaviorSubject(() => {}); 
+const reset$ = new BehaviorSubject(() => {}); 
+const autoInc$ = new BehaviorSubject(false); 
 
 const Update$ = merge(
-  inc$.pipe(map(() => (count) => Math.min(count + 1, 98))), // Increment operation
-  dec$.pipe(map(() => (count) => Math.max(count - 1, 0))), // Decrement operation
-  reset$.pipe(map(() => () => 0)), // Reset operation
+  inc$.pipe(map(() => (count) => Math.min(count + 1, 98))), 
+  dec$.pipe(map(() => (count) => Math.max(count - 1, 0))),
+  reset$.pipe(map(() => () => 0)),
   autoInc$.pipe(
     switchMap((enabled) =>
       enabled
